d964d96e90d5a2e29b113f0cce6c4776bc433515
Ноутбук ITSumnikoff Lumen <sasha-vasilchenko-1999@mail.ru>
2026-01-30 17:38:53 +0500

    ограничения в url в быстрых карточках

--- resources/views/partials/link-cards.blade.php
+++ resources/views/partials/link-cards.blade.php
@@ -72,8 +72,7 @@
                         @endif
                     </div>
 
-                    <div class="text-sm font-semibold text-gray-900" title="{{ e($card->title) }}">
-                        {{ e(\Illuminate\Support\Str::limit($card->title, 15)) }}</div>
+                    <div class="text-sm font-semibold text-gray-900">{{ $card->title }}</div>
                 </a>
 
                 <div class="text-xs text-gray-500 break-all mt-1" title="{{ e($card->url) }}">
